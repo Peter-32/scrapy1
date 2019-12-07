f584b6d6654a561e2efa9660038a601ba97fc67c
Peter-32 <peter@impactradius.com>
2019-12-07 15:35:49 -0800

    a

--- src/main/1.main.py
+++ src/main/1.main.py
@@ -12,7 +12,6 @@ except:
 os.mkdir("html")
 
 # start_urls = [
-#     'https://curvature.wd5.myworkdayjobs.com/External/1/refreshFacet/318c8bb6f553100021d223d9780d30be',
 #     'https://inogen.applicantpro.com/jobs/', # goleta only
 #     'https://careers.sonos.com/jobs/SearchJobs/?3_72_3=%5B%2215814%22%5D',
 #     'https://flexerasoftware.wd1.myworkdayjobs.com/FlexeraSoftware/1/refreshFacet/318c8bb6f553100021d223d9780d30be',
@@ -33,7 +32,7 @@ os.mkdir("html")
 
 
 class CJSpider(scrapy.Spider):
-    name = "job_listings"
+    name = "cj"
     start_urls = ['https://jobs.cj.com/jobs/city/santa-barbara#/']
 
     def parse(self, response):
@@ -44,17 +43,17 @@ class CJSpider(scrapy.Spider):
 
     def parse_listing(self, response):
         page = response.url.split("/")[-2]
-        filename = 'html/cj_{}.html'.format(page)
+        filename = 'html/{}_{}.html'.format(self.name, page)
         i = 2
         while os.path.exists(filename):
-            filename = "html/cj_{}_{}.html".format(page, i)
+            filename = "html/{}_{}_{}.html".format(self.name, page, str(i).zfill(3))
             i += 1
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file {}'.format(filename))
 
 class RaytheonSpider(scrapy.Spider):
-    name = "job_listings"
+    name = "raytheon"
     start_urls = ['https://jobs.raytheon.com/search-jobs/United%20States?orgIds=4679&ac=22812&alp=6252001&alt=2']
 
     def parse(self, response):
@@ -65,18 +64,39 @@ class RaytheonSpider(scrapy.Spider):
 
     def parse_listing(self, response):
         page = response.url.split("/")[-2]
-        filename = 'html/{}.html'.format(page)
+        if "search-jobs" in page:
+            return
+        filename = 'html/{}_{}.html'.format(self.name, page)
         i = 2
         while os.path.exists(filename):
-            filename = "html/raytheon_{}_{}.html".format(page, i)
+            filename = "html/{}_{}_{}.html".format(self.name, page, str(i).zfill(3))
             i += 1
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file {}'.format(filename))
 
+class InogenSpider(scrapy.Spider):
+    name = "inogen"
+    start_urls = ['https://inogen.applicantpro.com/jobs/']
 
+    def parse(self, response):
+        next_pages = response.css('.job-listings a::attr(href)').getall()
+        for href in next_pages:
+            yield response.follow(href, self.parse_listing)
+
+    def parse_listing(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'html/{}_{}.html'.format(self.name, page)
+        i = 2
+        while os.path.exists(filename):
+            filename = "html/{}_{}_{}.html".format(self.name, page, str(i).zfill(3))
+            i += 1
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file {}'.format(filename))
 
 process = CrawlerProcess()
 process.crawl(CJSpider)
 process.crawl(RaytheonSpider)
+process.crawl(InogenSpider)
 process.start() # the script will block here until all crawling jobs are finished
